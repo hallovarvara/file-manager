165346428188526f3ecced1632cdf922da42ad53
hallovarvara <hallovarvara@gmail.com>
2022-12-10 01:01:48 +0000

    feat: add readline module

--- src/modules/execute-command-stream.js
+++ src/modules/execute-command-stream.js
@@ -1,4 +1,3 @@
-import { Transform } from 'stream';
 import { exit } from './exit.js';
 import { help } from './help.js';
 import { executeUp } from './execute-up.js';
@@ -17,44 +16,38 @@ import { decompress } from './files/decompress.js';
 import { executeOsFunctionByArgument } from './execute-os-function-by-argument.js';
 import { STOP_COMMAND } from '../lib/constants/index.js';
 
-export const executeCommandStream = new Transform({
-    async transform(chunk, encoding, callback) {
-        const command = chunk.toString().trim();
-
-        if (command === STOP_COMMAND) {
-            exit();
-        } else if (command === 'help' || command === '.help') {
-            help();
-        } else if (command === 'up') {
-            executeUp();
-        } else if (command === 'ls') {
-            await getDirectoryContentList();
-        } else if (command.startsWith('cd')) {
-            executeCd(command);
-        } else if (command.startsWith('cat')) {
-            await read(command);
-        } else if (command.startsWith('add')) {
-            await add(command);
-        } else if (command.startsWith('rn')) {
-            await rename(command);
-        } else if (command.startsWith('cp')) {
-            await copy(command);
-        } else if (command.startsWith('mv')) {
-            await move(command);
-        } else if (command.startsWith('rm')) {
-            await remove(command);
-        } else if (command.startsWith('os')) {
-            await executeOsFunctionByArgument(command);
-        } else if (command.startsWith('hash')) {
-            await hash(command);
-        } else if (command.startsWith('compress')) {
-            await compress(command);
-        } else if (command.startsWith('decompress')) {
-            await decompress(command);
-        } else {
-            throwError({ isInputInvalid: true, showCurrentPath: true });
-        }
-
-        callback();
-    },
-});
+export const executeCommandStream = async (command) => {
+    if (command === STOP_COMMAND) {
+        exit();
+    } else if (command === 'help' || command === '.help') {
+        help();
+    } else if (command === 'up') {
+        executeUp();
+    } else if (command === 'ls') {
+        await getDirectoryContentList();
+    } else if (command.startsWith('cd')) {
+        executeCd(command);
+    } else if (command.startsWith('cat')) {
+        await read(command);
+    } else if (command.startsWith('add')) {
+        await add(command);
+    } else if (command.startsWith('rn')) {
+        await rename(command);
+    } else if (command.startsWith('cp')) {
+        await copy(command);
+    } else if (command.startsWith('mv')) {
+        await move(command);
+    } else if (command.startsWith('rm')) {
+        await remove(command);
+    } else if (command.startsWith('os')) {
+        await executeOsFunctionByArgument(command);
+    } else if (command.startsWith('hash')) {
+        await hash(command);
+    } else if (command.startsWith('compress')) {
+        await compress(command);
+    } else if (command.startsWith('decompress')) {
+        await decompress(command);
+    } else {
+        throwError({ isInputInvalid: true, showCurrentPath: true });
+    }
+};
--- src/modules/init-file-manager.js
+++ src/modules/init-file-manager.js
@@ -1,4 +1,4 @@
-import { pipeline } from 'stream';
+import readline from 'readline';
 import { executeCommandStream } from './execute-command-stream.js';
 import { welcomeUser } from './welcome-user.js';
 import { showCurrentPath } from '../lib/utils/show-current-path.js';
@@ -8,22 +8,34 @@ import { throwError } from '../lib/utils/throw-error.js';
 import { STOP_COMMAND } from '../lib/constants/index.js';
 
 export const initFileManager = async () => {
-    const readable = process.stdin;
-    const writable = process.stdin;
+    const readingLine = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    });
 
     welcomeUser();
 
+    write(`If you would like to exit, press "Ctrl + C" or type "${STOP_COMMAND}" on a new line and press Enter
+Type "help" or ".help" to get commands list`);
+
     showCurrentPath();
 
-    write(`If you would like to exit, press "Ctrl + C" or type "${STOP_COMMAND}" on a new line and press Enter
-Type "help" or ".help" to get commands list
-    `);
+    readingLine
+        .on('line', async (line) => {
+            const command = line.toString().trim();
+            // const [command, ...args] = lineToString.split(' ');
+            await executeCommandStream(command);
+        })
+        .on('error', (error) => {
+            throwError({ error, showCurrentPath: true });
+        })
+        .on('end', () => {
+            exit();
+        });
 
-    pipeline(readable, executeCommandStream, writable, (error) => {
-        throwError({ error });
-    });
-
-    process.on('SIGINT', () => {
-        exit();
+    process.stdin.on('keypress', (chunk, key) => {
+        if (key && key.name === 'c' && key.ctrl) {
+            exit();
+        }
     });
 };
